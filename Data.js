070cb379385e0e93a313cdc7d40135b608b58b16
zencyy <157678476+zencyy@users.noreply.github.com>
2024-12-01 22:08:06 +0800

    Add files via upload
    This is where all the product is stored.

---
+++ Data.js
@@ -0,0 +1,59 @@
+let shopItemsData = [
+  {
+    id: "razer",
+    name: "Razer Blackwidow Chroma",
+    price: 100,
+    desc: "",
+    img: "images/keyboard01.jpg",
+  },
+  {
+    id: "steel",
+    name: "SteelSeries Apex RGB Chroma",
+    price: 80,
+    desc: "",
+    img: "images/keyboard02.jpg",
+  },
+  {
+    id: "vector",
+    name: "Vector Rex Viper Chroma X100",
+    price: 115,
+    desc: "",
+    img: "images/keyboard03.jpg",
+  },
+  {
+    id: "cooler",
+    name: "Cooler Master CK5300 Chroma",
+    price: 120,
+    desc: "",
+    img: "images/keyboard04.jpg",
+  },
+  {
+    id: "razerhunts",
+    name: "Razer Huntsman Tournament",
+    price: 145,
+    desc: "",
+    img: "images/keyboard05.jpg",
+  },
+  {
+    id: "coolermaster",
+    name: "Cooler Master CK2300 Chroma",
+    price: 90,
+    desc: "",
+    img: "images/keyboard06.jpg",
+  },
+  {
+    id: "langtu",
+    name: "Langtu Membrane Backstrike",
+    price: 60,
+    desc: "",
+    img: "images/keyboard07.jpg",
+  },
+  {
+    id: "redragon",
+    name: "Redragon X300 Dragonborn Chroma",
+    price: 120,
+    desc: "",
+    img: "images/keyboard08.jpg",
+  },
+
+];
